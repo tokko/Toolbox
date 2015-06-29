f038d11b827579ce5b72d835181d016db754f42e
tokko <andreg@kth.se>
2015-06-29 06:11:41 +0200

    initial commit

---
+++ Toolbox/ToolboxExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace Toolbox
+{
+	public static class ToolboxExtensions
+	{
+		public static bool DeepEquals<T1, T2>(this T1 t1, T2 t2, bool allProperties = false)
+			where T1 : class
+			where T2 : class
+		{
+			var t1Props = t1.GetType().GetProperties();
+			var t2Props = t2.GetType().GetProperties();
+			if (allProperties && t1Props.Count() != t2Props.Count())
+				return false;
+			foreach (var propertyInfo in t1Props) {
+				var t2prop =
+					t2Props.SingleOrDefault(prop => prop.Name.Equals(propertyInfo.Name) && prop.GetType() == propertyInfo.GetType());
+				if(t2prop == null) continue;
+				var v1 = propertyInfo.GetValue(t1);
+				var v2 = t2prop.GetValue(t2);
+				if (propertyInfo.PropertyType.IsValueType || propertyInfo.PropertyType.IsPrimitive ||
+				    propertyInfo.PropertyType == typeof(string))
+					if (!v1.Equals(v2))
+						return false;
+					else continue;
+				var interfaces = propertyInfo.PropertyType.GetInterfaces();
+				if (interfaces.Contains(typeof (IEnumerable))) //object is enumerable
+				{
+					var e1 = ((IEnumerable) v1).GetEnumerator();
+					var e2 = ((IEnumerable) v2).GetEnumerator();
+					while(e1.MoveNext() && e2.MoveNext())
+							if(!e1.Current.DeepEquals(e2.Current, allProperties))
+								return false;
+					if (e1.MoveNext() || e2.MoveNext()) return false; //lists are not of equal length
+				}
+				else if (!v1.DeepEquals(v2, allProperties))
+					return false;
+			}
+			return true;
+		}
+	}
+}
---
+++ ToolboxTests/ToolboxExtensionsTests.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Toolbox;
+
+namespace ToolboxTests
+{
+	[TestFixture]
+	public class ToolboxExtensionsTests
+	{
+		[Test]
+		public void DeepEquals_AllPropertiesOneMissingProperty_NotEquals()
+		{
+			var a = new A {
+				a = "ga",
+				b = 123
+			};
+			var b = new B { a = "ga" };
+			var r = a.DeepEquals(b, true);
+			Assert.That(r, Is.False);
+		}
+
+		[Test]
+		public void DeepEquals_AllPropertiesAreNotEqual_NotEquals()
+		{
+			var a = new A {
+				a = "ga",
+				b = 123
+			};
+			var c = new C { a = a.a, b = 123456 };
+			var r = a.DeepEquals(c, true);
+			Assert.That(r, Is.False);
+		}
+
+		[Test]
+		public void DeepEquals_AllPropertiesAreEqual_Equals()
+		{
+			var a = new A {
+				a = "ga",
+				b = 123
+			};
+			var c = new C { a = a.a, b = a.b };
+			var r = a.DeepEquals(c, true);
+			Assert.That(r, Is.True);
+		}
+		[Test]
+		public void DeepEquals_IntersectionAreEqual_Equals()
+		{
+			var a = new A {
+				a = "ga",
+				b = 123
+			};
+			var b = new B { a = a.a };
+			var r = a.DeepEquals(b);
+			Assert.That(r, Is.True);
+		}
+
+		[Test]
+		public void DeepEquals_IntersectionAreNotEqual_NotEquals()
+		{
+			var a = new A {
+				a = "ga",
+				b = 123
+			};
+			var b = new B { a = "fwaga" };
+			var r = a.DeepEquals(b);
+			Assert.That(r, Is.False);
+		}
+
+		[Test]
+		public void DeepEquals_ObjectGraphAreEqual_Equals()
+		{
+			var d = new D {A = new A {a = "gf", b = 0}};
+			var d1 = new D{A = d.A};
+			var r = d.DeepEquals(d1);
+			Assert.That(r, Is.True);
+		}
+		
+		[Test]
+		public void DeepEquals_ObjectGraphAreEqual_NotEquals()
+		{
+			var d = new D {A = new A {a = "gf", b = 0}};
+			var d1 = new D{A = new A{a = "fa", b = 1}};
+			var r = d.DeepEquals(d1);
+			Assert.That(r, Is.False);
+		}
+
+		[Test]
+		public void DeepEquals_WithListOfObjectsAreEqual_Equals()
+		{
+			var e = new E<B> {List = new List<B> {new B {a = "gafa"}}};
+			var e1 = new E<B> { List = new List<B> { new B { a = "gafa" } } };
+			var r = e.DeepEquals(e1);
+			Assert.That(r, Is.True);
+		}
+
+		[Test]
+		public void DeepEquals_WithListOfObjectsAreNotEqual_NotEquals()
+		{
+			var e = new E<B> { List = new List<B> { new B { a = "gafa" } } };
+			var e1 = new E<B> { List = new List<B> { new B { a = "ga" } } };
+			var r = e.DeepEquals(e1);
+			Assert.That(r, Is.False);
+		}
+		[Test]
+		public void DeepEquals_WithListOfObjectsOfNotEqualLength_NotEquals()
+		{
+			var e = new E<B> { List = new List<B> { new B { a = "gafa" } } };
+			var e1 = new E<B> { List = new List<B> { new B { a = "gafa" }, new B { a = "ga" } } };
+			var r = e.DeepEquals(e1);
+			Assert.That(r, Is.False);
+		}
+
+		[Test]
+		public void DeepEquals_WithListOfObjectsOfNotEqualType_NotEquals()
+		{
+			var e = new E<A> { List = new List<A> { new A { a = "gafa" } } };
+			var e1 = new E<B> { List = new List<B> { new B { a = "gafa" }, new B { a = "ga" } } };
+			var r = e.DeepEquals(e1);
+			Assert.That(r, Is.False);
+		}
+
+		
+	}
+
+	public class A
+	{
+		public string a { get; set; }
+		public int b { get; set; }
+	}
+
+	public class B
+	{
+		public string a { get; set; }
+	}
+
+	public class C : A
+	{
+		
+	}
+
+	public class D
+	{
+		public A A { get; set; }
+	}
+
+	public class E<T>
+	{
+		public List<T> List { get; set; }
+	}
+}

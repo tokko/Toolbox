61881ebf20b48e55679e19cc0d8d279e3b20d6fe
tokko <andreg@kth.se>
2015-08-29 12:40:33 +0200

    test that strings with values are ignored

--- ToolboxTests/NullCullExtensionTests.cs
+++ ToolboxTests/NullCullExtensionTests.cs
@@ -19,6 +19,14 @@ namespace ToolboxTests
 		}
 
 		[Test]
+		public static void NullCull_StringAlreadyHasValue_IsIgnored()
+		{
+			var st = new StringTypes {S = "test"};
+			st.NullCull();
+			Assert.That(st.S, Is.EqualTo("test"));
+		}
+
+		[Test]
 		public static void NullCull_ComplextTypeAlreadyHasValue_IsIgnored()
 		{
 			var ct = new ComplexTypes{PrimitiveTypes = new PrimitiveTypes{i = 5}};
@@ -27,6 +35,10 @@ namespace ToolboxTests
 			Assert.That(ct.PrimitiveTypes.i, Is.EqualTo(5));
 		}
 
+		private class StringTypes
+		{
+			public string S { get; set; }
+		}
 		private class ComplexTypes
 		{
 			public PrimitiveTypes PrimitiveTypes { get; set; }

8cb31b01ccaf5ec4152934474547abdc90f6ade9
tokko <andreg@kth.se>
2015-08-29 12:35:04 +0200

    nullcull and test for primitives

---
+++ Toolbox/NullCullExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Toolbox
+{
+	public static class NullCullExtensions
+	{
+
+		public static void NullCull<T>(this T src) where T : class
+		{
+
+		}
+	}
+}
---
+++ ToolboxTests/NullCullExtensionTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Toolbox;
+
+namespace ToolboxTests
+{
+	[TestFixture]
+	public class NullCullExtensionTests
+	{
+		[Test]
+		public static void NullCull_PrimitiveTypeAlreadyHasValue_IsIgnored()
+		{
+			var pt = new PrimitiveTypes{i = 5};
+			pt.NullCull();
+			Assert.AreNotEqual(pt.i, 0);
+		}
+
+		private class PrimitiveTypes
+		{
+			public int i { get; set; }
+		}
+	}
+}
